8c894661cbfc39f1d94a8520cdeb92e2bca44885
pavlo.rodriguez@gmail.com <pavlo.rodriguez@gmail.com@ec8c976c-d0b6-afce-9db9-7f7633520312>
2011-12-15 19:02:53 +0000

    Se agregó la paginación a nivel de base de datos. se actualizó la versión de rich faces por la 3 (antes era la 2.2)

--- trunk/logistica/logistica_jar/src/main/java/logistica/query/ClienteQuery.java
+++ trunk/logistica/logistica_jar/src/main/java/logistica/query/ClienteQuery.java
@@ -1,5 +1,22 @@
 package logistica.query;
 
 public class ClienteQuery extends BaseQuery {
+	private String nombre;
 
+	public ClienteQuery(Long id, String nombre) {
+		super(id);
+		this.nombre = nombre;
+	}
+
+	public ClienteQuery() {
+		this(null, null);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
