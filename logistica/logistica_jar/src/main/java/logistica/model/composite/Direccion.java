d1698614aa29428761680aeb54ab545d04669dc8
pavlo.rodriguez <pavlo.rodriguez@gmail.com>
2011-12-30 20:14:42 +0000

    se agregó la el objeto direccion

---
+++ logistica/logistica_jar/src/main/java/logistica/model/composite/Direccion.java
@@ -0,0 +1,95 @@
+package logistica.model.composite;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import logistica.model.Localidad;
+
+@Embeddable
+public class Direccion {
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "localidadID")
+	private Localidad localidad;
+
+	@Column(length = 200)
+	private String calle;
+
+	@Column(length = 20)
+	private String altura;
+
+	@Column(length = 20)
+	private String piso;
+
+	@Column(length = 20)
+	private String departamento;
+
+	@Column(length = 20)
+	private String codigoPostal;
+
+	public Direccion(Localidad localidad, String calle, String altura,
+			String piso, String departamento, String codigoPostal) {
+		super();
+		this.localidad = localidad;
+		this.calle = calle;
+		this.altura = altura;
+		this.piso = piso;
+		this.departamento = departamento;
+		this.codigoPostal = codigoPostal;
+	}
+
+	public Direccion() {
+		this(null, null, null, null, null, null);
+	}
+
+	public Localidad getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(Localidad localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getCalle() {
+		return calle;
+	}
+
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+
+	public String getAltura() {
+		return altura;
+	}
+
+	public void setAltura(String altura) {
+		this.altura = altura;
+	}
+
+	public String getPiso() {
+		return piso;
+	}
+
+	public void setPiso(String piso) {
+		this.piso = piso;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+}
