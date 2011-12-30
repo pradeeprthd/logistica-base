3ee5e977ee14300b56edd2cc01fce009ee80a9b4
pavlo.rodriguez@gmail.com <pavlo.rodriguez@gmail.com@ec8c976c-d0b6-afce-9db9-7f7633520312>
2011-12-30 15:19:30 +0000

    se cambio el rol ADMIN_USER por ROL_ADMIN. Se le agregó estilo al formulario de login

--- trunk/logistica/logistica_jar/src/main/resources/inicial.sql
+++ trunk/logistica/logistica_jar/src/main/resources/inicial.sql
@@ -1,4 +1,4 @@
 -- usuario admin
-INSERT INTO logistica.usuario(id, contrasenia, enable, usuario) VALUES (9999999, '7c4a8d09ca3762af61e59520943dc26494f8941b', true, 'admin2');
-INSERT INTO logistica.rolenum(usuario_id, rolenumlist) VALUES (9999999, 'ADMIN_USER');
+INSERT INTO logistica.usuario(id, contrasenia, enable, usuario) VALUES (9999999, '7c4a8d09ca3762af61e59520943dc26494f8941b', true, 'admin');
+INSERT INTO logistica.rolenum(usuario_id, rolenumlist) VALUES (9999999, 'ROLE_ADMIN');
 INSERT INTO logistica.rolenum(usuario_id, rolenumlist)VALUES (9999999, 'ROLE_USER');
