d85e06ec7ba997c167e7b543b23357d32644854f
pavlo.rodriguez <pavlo.rodriguez@gmail.com>
2012-01-09 20:38:07 +0000

    Se intentó poner la query cache. y el indice en el tabview

--- logistica/logistica_jar/src/main/java/logistica/common/dao/ProvinciaDAOImpl.java
+++ logistica/logistica_jar/src/main/java/logistica/common/dao/ProvinciaDAOImpl.java
@@ -1,12 +1,33 @@
 package logistica.common.dao;
 
+import java.util.List;
+
 import logistica.model.Provincia;
 import logistica.query.BaseQuery;
+
+import org.hibernate.Query;
+import org.springframework.dao.DataAccessException;
 
 public class ProvinciaDAOImpl extends BaseHibernateDAO<Provincia, BaseQuery> {
 
 	@Override
 	public Class<Provincia> getModelClass() {
 		return Provincia.class;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Provincia> getList() throws DataAccessException {
+		Query query = getSession().createQuery("from Provincia");
+		query.setCacheable(true);
+		List<Provincia> objectList = query.list();
+
+		// DetachedCriteria criteria =
+		// DetachedCriteria.forClass(Provincia.class);
+		// // criteria.getExecutableCriteria(getSession()).setCacheable(true);
+		// HibernateTemplate ht = getHibernateTemplate();
+		// ht.setCacheQueries(true);
+		// List<Provincia> objectList = ht.findByCriteria(
+		// criteria);
+		return objectList;
 	}
 }
